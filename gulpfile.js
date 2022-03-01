0746378d53188bb0e9764a0986c9fc947824d5e5
nogueiraac <240700ac@gmail.com>
2022-03-01 20:25:31 -0300

    Adding some plugins in css/ and js/ folders.

--- gulpfile.js
+++ gulpfile.js
@@ -21,9 +21,17 @@ function compileSass() {
     .pipe(gulp.dest("css/"))
     .pipe(browserSync.stream());
 }
-
 /* Tarefa do sass*/
 gulp.task("sass", compileSass);
+
+function pluginCSS() {
+  return gulp
+    .src("css/lib/*.css")
+    .pipe(concat("plugins.css"))
+    .pipe(gulp.dest("css/"))
+    .pipe(browserSync.stream());
+}
+gulp.task("plugincss", pluginCSS);
 
 /* Unindo todos os arquivos javascripts em um único arquivo*/
 function gulpJs() {
@@ -42,6 +50,16 @@ function gulpJs() {
 /* Tarefa do alljs*/
 gulp.task("alljs", gulpJs);
 
+/* Unindo todos os plugins javascripts em um único arquivo*/
+function pluginsJs() {
+  return gulp
+    .src(["js/lib/aos.min.js", "js/lib/swiper.min.js"])
+    .pipe(concat("plugins.js"))
+    .pipe(gulp.dest("js/"))
+    .pipe(browserSync.stream());
+}
+/* Tarefa do pluginjs*/
+gulp.task("pluginjs", pluginsJs);
 /* Função do browserSync*/
 function browser() {
   browserSync.init({
@@ -58,10 +76,22 @@ function watch() {
   gulp.watch("scss/*.scss", compileSass);
   gulp.watch("*.html").on("change", browserSync.reload);
   gulp.watch("/js/scripts/*.js", gulpJs);
+  gulp.watch("/js/lib/*.js", pluginsJs);
+  gulp.watch("/css/lib/*.css", pluginCSS);
 }
 
 /* Tarefa do watch*/
 gulp.task("watch", watch);
 
 /* Tarefa do padrão que executa o watch e o browser-sync*/
-gulp.task("default", gulp.parallel("watch", "browser-sync", "alljs", "sass"));
+gulp.task(
+  "default",
+  gulp.parallel(
+    "watch",
+    "browser-sync",
+    "alljs",
+    "sass",
+    "pluginjs",
+    "pluginscss"
+  )
+);
