61fdb98aa95f2026e6ab88fe3360ab3a3ceb6eaa
nogueiraac <240700ac@gmail.com>
2022-03-01 15:20:51 -0300

    Adding browse-sync in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const autoprefixer = require("gulp-autoprefixer");
+const browserSync = require("browser-sync").create();
 
+/* Compilando o sass, adicionando autoprefixed e atualizando a página*/
 function compileSass() {
   return gulp
     .src("scss/*.scss")
@@ -12,13 +14,32 @@ function compileSass() {
         cascade: false,
       })
     )
-    .pipe(gulp.dest("css/"));
+    .pipe(gulp.dest("css/"))
+    .pipe(browserSync.stream());
 }
 
+/* Tarefa do sass*/
 gulp.task("sass", compileSass);
 
+/* Função do browserSync*/
+function browser() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
+}
+/* Tarefa do browser-sync*/
+gulp.task("browser-sync", browser);
+
+/* Função do watch para alterações em scss e html*/
 function watch() {
-  gulp.watch("scss/*.scss", gulp.series("sass"));
+  gulp.watch("scss/*.scss", compileSass);
+  gulp.watch("*.html").on("change", browserSync.reload);
 }
 
-gulp.task("default", watch);
+/* Tarefa do watch*/
+gulp.task("watch", watch);
+
+/* Tarefa do padrão que executa o watch e o browser-sync*/
+gulp.task("default", gulp.parallel("watch", "browser-sync"));
